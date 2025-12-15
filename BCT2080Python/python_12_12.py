8d758fa9d200d76c00cc43e06b5d61b1d1e6efd8
Yolisa Bhujel <yolisabhujel@gmail.com>
2025-12-15 15:45:23 +0545

    first commit

---
+++ BCT2080Python/python_12_12.py
@@ -0,0 +1,31 @@
+# list knowledge
+my_list=[1,2,3,4,5]
+
+
+
+
+    # tuple knowledge
+my_tuple=(1,2,3,4,5)
+    
+
+#learn dictionary
+my_dict={}
+breakpoint()
+
+# clear() removes all the elements from the dictionary
+cleared_dict = my_dict.clear()
+#copy() returns a shallow copy of the dictionary
+# fromkeys() creates a new dictionary with keys from iterable and values set to value
+# get() returns the value of the specified key
+get_value = my_dict.get('key', 'default_value')
+# items() returns a view object that displays a list of a dictionary's key-value tuple pairs
+# keys() returns a view object that displays a list of all the keys in the dictionary
+# pop() removes the element with the specified key
+# popitem() removes the last inserted key-value pair
+# setdefault() returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update() updates the dictionary with the specified key-value pairs
+# values() returns a view object that displays a list of all the values in the dictionary
+
+
+ 
+               
